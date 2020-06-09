173e6505bee6a735ddc6f14da079f387c23d04fc
Vonrentsalot <vonrentsalot@gmail.com>
2020-06-09 09:32:35 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nicholas von Rentzell and Sonja Rebarber
+ *         cbourke@cse.unl.edu
+ * Date: 2020/06/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nicholas von Rentzell and Sonja Rebarber\n");
+
+  return 0;
+}
